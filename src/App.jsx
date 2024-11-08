8b36677a8a6d55fe66004ed33af1594b4548f45d
numi8462 <yhk8462@naver.com>
2024-11-08 16:25:21 +0900

    added features and footer

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Hightlights from './components/Highlights'
 import Model from './components/Model'
+import Features from './components/Features'
+import HowItWorks from './components/HowItWorks'
+import Footer from './components/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,6 +17,9 @@ function App() {
       <Hero/>
       <Hightlights/>
       <Model/>
+      <Features/>
+      <HowItWorks/>
+      <Footer/>
     </main>
     
   )
